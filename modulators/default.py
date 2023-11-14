2ecb5607c3eac59bcd4fe72104678f2d5b0947f1
Kb1p <dmitriev.k.s@gmail.com>
2023-11-14 13:04:57 +0300

    Implemented all required modulation signal shapes

--- modulators/default.py
+++ modulators/default.py
@@ -1,25 +1,33 @@
 import math
 
+# Set frequency here
+freqHz = 220
+
+p = 1.0 / freqHz
+
 def solidNoise(t, n):
     return n
 
 def meander(t, n):
-    freqHz = 500
-    return 1.0 if math.floor(t * freqHz) % 2 == 0 else 0.0
+    return 1.0 if math.floor(t * freqHz * 2.0) % 2 == 1 else 0.0
 
 def noiseMeander(t, n):
     return n * meander(t, 0)
 
 def sine(t, n):
-    freqHz = 1000
-    return (math.sin(t * freqHz * math.pi) + 1.0) / 2.0
+    return (math.sin(t * freqHz * math.pi * 2.0) + 1.0) / 2.0
 
 def noiseSine(t, n):
     return n * sine(t, 0)
 
+def sawtooth(t, n):
+    return math.fmod(t, p) / p
+
+def noiseSawtooth(t, n):
+    return n * sawtooth(t, 0)
+
 def triangle(t, n):
-    freqHz = 500
-    return t**2 if math.floor(t * freqHz) % 2 == 0 else 1.0 - t
+    return 2.0 / p * abs(math.fmod(t + p / 2.0, p) - p / 2.0)
 
 def noiseTriangle(t, n):
     return n * triangle(t, 0)
